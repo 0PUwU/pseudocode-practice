0ad00cdc239e37cb4b8ec10e5082986117ac0523
0PUwU <PerezOw970@northwested.org>
2024-09-24 11:19:00 -0400

    w

---
+++ main2.py
@@ -0,0 +1,12 @@
+# Owen Perez
+# 9/24/24
+# Pseudocode Practice
+
+f_name = input(f"What is your first name?: ")
+quiz1 = int(input(f"How many points on first quiz: "))
+quiz2 = int(input(f"How many points did you get on second quiz: "))
+quiz3 = int(input(f"How many points on third quiz: "))
+
+total_score = 3 / quiz1 + quiz2 + quiz3
+
+print(f"You name is {f_name} and your theree scores are, {quiz1}, {quiz2}, {quiz3}, and the avarage score is {total_score} ")
