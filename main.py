ae0502e64c4fd0138ac79e200042ce586b803a3a
0PUwU <PerezOw970@northwested.org>
2024-09-24 10:59:23 -0400

    fixed

---
+++ main.py
@@ -0,0 +1,11 @@
+# Owen Perez
+# 9/24/24
+# Pseudocode Practice
+
+f_name = input(f"What is your first name?: ")
+dist_driven = float(input(f"How many miles did you drive today?: "))
+gal_consumed = float(input(f"How many gallons of gas did you use today?: "))
+
+total_mpg = dist_driven / gal_consumed
+
+print(f"You drove {dist_driven} miles, used {gal_consumed} gallons of gas, and your miles per gallon is {total_mpg}")
